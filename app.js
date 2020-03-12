736015d82e6591646c2a00b5f1cbb31d5dea338d
Nedim Karčić <nedimkarcic52@gmail.com>
2020-03-12 23:13:22 +0100

    cleared code

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// const title = document.querySelector("#title");
-// const author = document.querySelector("#author");
-// const isbn = document.querySelector("#isbn");
-// const form = document.querySelector("#input-form");
-
 class Book {
 	constructor (form, title, author, isbn){
 		this.form = document.querySelector("#input-form");
@@ -23,18 +18,11 @@ class Book {
 	let tdata2 = document.createElement("td");
 	let tdata3 = document.createElement("td");
 	let deleteBook = document.createElement("td");
-	// let icon = "<i class='fas fa-trash'></i>";
-
-
-
-	// <i class="fas fa-trash"></i>
 
 	tdata1.innerHTML = this.title.value;
 	tdata2.innerHTML = this.author.value;
 	tdata3.innerHTML = this.isbn.value;
 	deleteBook.innerHTML = "<i class='fas fa-trash'></i>";
-	// icon.classList.add("fas");
-	// icon.classList.add("fa-trash");
 
 	row.appendChild(tdata1);
 	row.appendChild(tdata2);
@@ -49,33 +37,5 @@ class Book {
 
 };
 
-//instanca ima parametre; šta ako nam input daje argumente, a onda se oni prebacuju u tabelu
 let book = new Book();
 console.log(title.value);
-
-
-
-
-
-
-// function bookSubmit (e){
-// 	e.preventDefault();
-
-// 	let table = document.querySelector("#table");
-// 	let row = document.createElement("tr");
-// 	let tdata1 = document.createElement("td");
-// 	let tdata2 = document.createElement("td");
-// 	let tdata3 = document.createElement("td");
-
-// 	tdata1.innerHTML = title.value;
-// 	tdata2.innerHTML = author.value;
-// 	tdata3.innerHTML = isbn.value;
-
-// 	row.appendChild(tdata1);
-// 	row.appendChild(tdata2);
-// 	row.appendChild(tdata3);
-
-// 	table.appendChild(row);
-// }
-
-// form.addEventListener('submit', bookSubmit);
